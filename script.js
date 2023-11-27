e2633f63fd1c6563d0aadfe87a8a4ba6ee0d59dc
austwhit <146144355+austwhit@users.noreply.github.com>
2023-11-27 01:18:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 async function getRandomContent() {
     try {
-        // Determine whether to fetch a random fish gif or a random Colombia gif
         const randomChoice = Math.random() < 0.5 ? 'fish' : 'Colombia';
-        
-        // Fetch the random content based on the choice
-        const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=pvyh7KDSwxvDlDN5oG6USLvuLrNj5ZSG&tag=${randomChoice}`);
+        const apiKey = 'pvyh7KDSwxvDlDN5oG6USLvuLrNj5ZSG';
+        const apiUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${randomChoice}`;
+
+        console.log('API URL:', apiUrl);
+
+        const response = await fetch(apiUrl);
         const data = await response.json();
 
-        // Extract the URL of the gif from the data
-        const gifUrl = data.data.image_original_url;
+        console.log('API Response:', data);
 
-        // Display the content on the webpage
+        const gifUrl = data.data.image_original_url;
         displayContent(gifUrl);
     } catch (error) {
         console.error('Error fetching content:', error);
     }
 }
+
 
 function displayContent(gifUrl) {
     // Display the gif
