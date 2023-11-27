f794774786de0b8d6c9338f932bb7b79a5d66fd5
austwhit <146144355+austwhit@users.noreply.github.com>
2023-11-27 01:13:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
 async function getRandomContent() {
-        try {
-            // Fetch both the random quote and cat gif concurrently using async/await
-            const [quoteResponse, catGifResponse] = await Promise.all([
-                fetch('https://api.quotable.io/random'),
-                fetch('https://api.thecatapi.com/v1/images/search?mime_types=gif')
-            ]);
+    try {
+        // Determine whether to fetch a random fish gif or a random Colombia gif
+        const randomChoice = Math.random() < 0.5 ? 'fish' : 'Colombia';
+        
+        // Fetch the random content based on the choice
+        const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=pvyh7KDSwxvDlDN5oG6USLvuLrNj5ZSG&tag=${randomChoice}`);
+        const data = await response.json();
 
-            const [quoteData, catGifData] = await Promise.all([
-                quoteResponse.json(),
-                catGifResponse.json()
-            ]);
+        // Extract the URL of the gif from the data
+        const gifUrl = data.data.image_original_url;
 
-            // Extract the URL of the cat gif from the catGifData
-            const catGifUrl = catGifData[0].url;
-
-            // Display the quote and cat gif on the webpage
-            displayContent(quoteData, catGifUrl);
-        } catch (error) {
-            console.error('Error fetching content:', error);
-        }
+        // Display the content on the webpage
+        displayContent(gifUrl);
+    } catch (error) {
+        console.error('Error fetching content:', error);
     }
+}
 
-    function displayContent(quote, catGifUrl) {
-        // Display the quote
-        const quoteContainer = document.getElementById('quote-container');
-        quoteContainer.innerHTML = '';
-        const quoteText = document.createElement('p');
-        quoteText.textContent = `"${quote.content}"`;
-        const quoteAuthor = document.createElement('p');
-        quoteAuthor.textContent = `- ${quote.author}`;
-        quoteContainer.appendChild(quoteText);
-        quoteContainer.appendChild(quoteAuthor);
+function displayContent(gifUrl) {
+    // Display the gif
+    const gifContainer = document.getElementById('gif-container');
+    gifContainer.innerHTML = '';
+    const gifImage = document.createElement('img');
+    gifImage.src = gifUrl;
+    gifImage.alt = 'Random Gif';
+    gifContainer.appendChild(gifImage);
+}
 
-        // Display the cat gif
-        const catContainer = document.getElementById('cat-container');
-        catContainer.innerHTML = '';
-        const catImage = document.createElement('img');
-        catImage.src = catGifUrl;
-        catImage.alt = 'Random Cat Gif';
-        catContainer.appendChild(catImage);
-    }
