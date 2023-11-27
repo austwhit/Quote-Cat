d24c7ffd07be796592d85faa96a006dbc8d25c70
austwhit <146144355+austwhit@users.noreply.github.com>
2023-11-26 23:10:18 -0500

    fish script.js

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
+const TENOR_API_KEY = 'YOUR_TENOR_API_KEY';
+
 async function getRandomContent() {
-        try {
-            // Fetch both the random quote and cat gif concurrently using async/await
-            const [quoteResponse, catGifResponse] = await Promise.all([
-                fetch('https://api.quotable.io/random'),
-                fetch('https://api.thecatapi.com/v1/images/search?mime_types=gif')
-            ]);
+    try {
+        // Fetch both the random quote and fish meme gif concurrently using async/await
+        const [quoteResponse, fishMemeGifResponse] = await Promise.all([
+            fetch('https://api.quotable.io/random'),
+            fetch(`https://g.tenor.com/v1/search?q=fish-meme&key=${TENOR_API_KEY}`)
+        ]);
 
-            const [quoteData, catGifData] = await Promise.all([
-                quoteResponse.json(),
-                catGifResponse.json()
-            ]);
+        const [quoteData, fishMemeGifData] = await Promise.all([
+            quoteResponse.json(),
+            fishMemeGifResponse.json()
+        ]);
 
-            // Extract the URL of the cat gif from the catGifData
-            const catGifUrl = catGifData[0].url;
+        // Extract the URL of the fish meme gif from the fishMemeGifData
+        const fishMemeGifUrl = fishMemeGifData.results[0].media[0].gif.url;
 
-            // Display the quote and cat gif on the webpage
-            displayContent(quoteData, catGifUrl);
-        } catch (error) {
-            console.error('Error fetching content:', error);
-        }
+        // Display the quote and fish meme gif on the webpage
+        displayContent(quoteData, fishMemeGifUrl);
+    } catch (error) {
+        console.error('Error fetching content:', error);
     }
+}
 
-    function displayContent(quote, catGifUrl) {
-        // Display the quote
-        const quoteContainer = document.getElementById('quote-container');
-        quoteContainer.innerHTML = '';
-        const quoteText = document.createElement('p');
-        quoteText.textContent = `"${quote.content}"`;
-        const quoteAuthor = document.createElement('p');
-        quoteAuthor.textContent = `- ${quote.author}`;
-        quoteContainer.appendChild(quoteText);
-        quoteContainer.appendChild(quoteAuthor);
+function displayContent(quote, fishMemeGifUrl) {
+    // Display the quote
+    const quoteContainer = document.getElementById('quote-container');
+    quoteContainer.innerHTML = '';
+    const quoteText = document.createElement('p');
+    quoteText.textContent = `"${quote.content}"`;
+    const quoteAuthor = document.createElement('p');
+    quoteAuthor.textContent = `- ${quote.author}`;
+    quoteContainer.appendChild(quoteText);
+    quoteContainer.appendChild(quoteAuthor);
 
-        // Display the cat gif
-        const catContainer = document.getElementById('cat-container');
-        catContainer.innerHTML = '';
-        const catImage = document.createElement('img');
-        catImage.src = catGifUrl;
-        catImage.alt = 'Random Cat Gif';
-        catContainer.appendChild(catImage);
-    }
+    // Display the fish meme gif
+    const fishMemeContainer = document.getElementById('fish-meme-container');
+    fishMemeContainer.innerHTML = '';
+    const fishMemeImage = document.createElement('img');
+    fishMemeImage.src = fishMemeGifUrl;
+    fishMemeImage.alt = 'Fish Meme GIF';
+    fishMemeContainer.appendChild(fishMemeImage);
+}
